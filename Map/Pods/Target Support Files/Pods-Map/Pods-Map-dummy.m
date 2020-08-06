60735fa2b62b66a44c54a4bdc43c856e65b39a49
peter <h78823462@gmail.com>
2020-08-06 10:35:33 +0800

    rename project]

--- Map/Pods/Target Support Files/Pods-Map/Pods-Map-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Map : NSObject
-@end
-@implementation PodsDummy_Pods_Map
-@end
--- Map/Pods/Target Support Files/Pods-MusicPlayer/Pods-MusicPlayer-umbrella.h
+++ Map/Pods/Target Support Files/Pods-MusicPlayer/Pods-MusicPlayer-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double Pods_MapVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_MapVersionString[];
+FOUNDATION_EXPORT double Pods_MusicPlayerVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_MusicPlayerVersionString[];
 
